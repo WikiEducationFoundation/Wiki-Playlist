d64074e25c60e26aa356f61a1ce8f75465ff3232
hudakdidit <jonathan@wintr.us>
2016-01-04 19:38:48 -0800

    Temporarily disabling redux debugger for heroku deploy

--- app/react/containers/Root.js
+++ app/react/containers/Root.js
@@ -1,5 +1,6 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Root.prod');
-} else {
-  module.exports = require('./Root.dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./Root.prod');
+// } else {
+  
+// }
+module.exports = require('./Root.dev');
