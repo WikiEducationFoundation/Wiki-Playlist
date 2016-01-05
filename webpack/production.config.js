75e9987a90cdb20ee770654933a60ba12685f706
hudakdidit <jonathan@wintr.us>
2016-01-04 19:34:46 -0800

    Added NODE_ENV variable to exclude redux debugger in production

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,3 +1,12 @@
+const webpack = require('webpack');
 const config = require('./../webpack.config');
+
+config.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': 'production'
+    }
+  })
+)
 
 module.exports = config;
