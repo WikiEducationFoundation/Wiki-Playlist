a9b5bda61da4a6c6f546dc94256dce897e12867a
hudakdidit <jonathan@wintr.us>
2016-01-04 19:40:28 -0800

    Ugh

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,12 +1,12 @@
 const webpack = require('webpack');
 const config = require('./../webpack.config');
 
-config.plugins.push(
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': 'production'
-    }
-  })
-)
+// config.plugins.push(
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': 'production'
+//     }
+//   })
+// )
 
 module.exports = config;
