6ae07bbb14b410ebaea16dacb1981b7979d774b8
hudakdidit <jonathan@wintr.us>
2016-01-06 14:27:16 -0800

    ImageSelector updates
    Image highlighting
    Select Image navigates back to playlist editor

--- app/react/components/ImageSelector.js
+++ app/react/components/ImageSelector.js
@@ -1,11 +1,46 @@
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { setArticleImage } from '../actions';
 
 
+class ImageSelector extends React.Component {
+
+  render() {
+    return(
+      <div>
+        Image selector
+        <div className='flex'>{this._images()}</div>
+        <Link className='btn btn-primary' to='/playlist/new'>Select Image</Link>
+      </div>
+    )
+  }
+
+  _images() {
+    const {editingArticle, articles} = this.props.Playlist;
+    const images = articles[editingArticle].images;
+    const currentImage = articles[editingArticle].image;
+    let _images = [];
+    images.map(img => {
+      const selected = (img === currentImage ? true : false);
+      _images.push(<ArticleImage key={img} 
+                                 dispatch={this.props.dispatch} 
+                                 articleIndex={editingArticle} 
+                                 src={img} 
+                                 selected={selected}/>)
+    });
+    return _images;
+  }
+}
+
+export default connect( state => {return state})(ImageSelector);
+
+
+/* ArticleImage for ImageSelector
+--------------------------------------------- */
 class ArticleImage extends React.Component {
   render() {
     return (
-      <div onClick={this.handleClick.bind(this)}>
+      <div className={'p1' + (this.props.selected ? ' bg-aqua' : '')}  onClick={this.handleClick.bind(this)}>
         <img src={this.props.src}/>
       </div>
     )
@@ -16,27 +51,3 @@ class ArticleImage extends React.Component {
     dispatch(setArticleImage(articleIndex, src));
   }
 }
-
-
-class ImageSelector extends React.Component {
-  render() {
-    return(
-      <div>
-        Image selector
-        <div className='flex'>{this._images()}</div>
-      </div>
-    )
-  }
-
-  _images() {
-    const {editingArticle, articles} = this.props.Playlist;
-    const images = articles[editingArticle].images;
-    let _images = [];
-    images.map(img => {
-      _images.push(<ArticleImage key={img} dispatch={this.props.dispatch} articleIndex={editingArticle} src={img} />)
-    });
-    return _images;
-  }
-}
-
-export default connect( state => {return state})(ImageSelector);
