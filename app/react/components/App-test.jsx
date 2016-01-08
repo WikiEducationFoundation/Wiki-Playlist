4d5fd7c832a080c4d07b56d26e0a6bbf80fb55f7
hudakdidit <jonathan@wintr.us>
2016-01-08 12:17:56 -0800

    Setup initial react component test with karma

---
+++ app/react/components/App-test.jsx
@@ -0,0 +1,14 @@
+import React from 'react/addons';
+const TestUtils = React.addons.TestUtils;
+import expect from 'expect';
+import App from './App';
+import configureStore from '../store/configureStore';
+const store = configureStore();
+
+describe('App', () => {
+  const component = TestUtils.renderIntoDocument(<App store={store} />);
+
+  it('renders', () => {
+    expect(TestUtils.findRenderedComponentWithType(component, App)).toExist();
+  });
+});
