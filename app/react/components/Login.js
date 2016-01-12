d61f95751fccae24591725a5ff620c82cc5a1d18
hudakdidit <jonathan@wintr.us>
2016-01-12 12:23:20 -0800

    Don't render login links if already logged in

--- app/react/components/Login.js
+++ app/react/components/Login.js
@@ -1,12 +1,25 @@
 import { Link } from 'react-router';
-export default class Login extends React.Component {
+import { connect } from 'react-redux';
+
+class Login extends React.Component {
   render() {
+    const { logged_in } = this.props;
+    return (logged_in ? this.loggedIn() : this.loginLinks())
+  }
+
+  loggedIn() {
+    return <div>You are logged in</div>
+  }
+
+  loginLinks() {
     return (
       <div>
         <div><a data-popup="Login with Mediawiki Account" href="/users/auth/mediawiki">Sign in with Mediawiki</a></div>
         <div><a data-popup="Login with Twitter" href="/users/auth/twitter">Sign in with Twitter</a></div>
         <div><a data-popup="Login with Facebook" href="/users/auth/facebook">Sign in with Facebook</a></div>
       </div>
-    )
+    );
   }
 }
+
+export default connect( state => {return state.Account})(Login)
