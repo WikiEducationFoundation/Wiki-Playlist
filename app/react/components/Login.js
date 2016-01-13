d799bb4cb4ee12c91ec6129f61ebd37f8845a145
hudakdidit <jonathan@wintr.us>
2016-01-13 14:50:09 -0800

    Renamed Wikipedia Login Link

--- app/react/components/Login.js
+++ app/react/components/Login.js
@@ -14,7 +14,7 @@ class Login extends React.Component {
   loginLinks() {
     return (
       <div>
-        <div><a data-popup="Login with Mediawiki Account" href="/users/auth/mediawiki">Sign in with Mediawiki</a></div>
+        <div><a data-popup="Login with Wikipedia Account" href="/users/auth/mediawiki">Sign in with Mediawiki</a></div>
         <div><a data-popup="Login with Twitter" href="/users/auth/twitter">Sign in with Twitter</a></div>
         <div><a data-popup="Login with Facebook" href="/users/auth/facebook">Sign in with Facebook</a></div>
       </div>
