c4c42a8db521ce094d779140973958018283778a
hudakdidit <jonathan@wintr.us>
2016-02-01 11:51:56 -0800

    Removed email index from User table

---
+++ spec/controllers/callbacks_controller.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe CallbacksController do
+
+  before do 
+    request.env["devise.mapping"] = Devise.mappings[:user] # If using Devise
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter] 
+  end
+
+end
