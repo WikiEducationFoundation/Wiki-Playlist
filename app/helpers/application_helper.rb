3a07a2fb2d6184fafa82f00c2dfafd2ce814973a
hudakdidit <jonathan@wintr.us>
2016-02-04 12:30:08 -0800

    Refinements to Share Image #41

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,10 @@ module ApplicationHelper
     truncate(text, :length => 300, :omission => '...')
   end
 
+  def truncate_username(text)
+    truncate(text, :length => 20, :omission => '...')
+  end
+
   def playlist_caption(caption)
     if caption.nil? || caption.empty?
       return "Share the joy of knowledge"
