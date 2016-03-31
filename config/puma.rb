bc586212d975dc5a08a5fa4bad3916be5c5d7ef5
Sage Ross <ragesoss@gmail.com>
2016-03-31 11:24:39 -0700

    Update puma.rb to match the working config on the server.
    The jobs don't get run without that Que.mode line, which does not get generated by `cap production puma:config`.

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,35 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+#!/usr/bin/env puma
+# This should be copied to shared/puma.rb for capistrano puma commands to work
+
+directory '/var/www/Wiki-Playlist/current'
+rackup "/var/www/Wiki-Playlist/current/config.ru"
+environment 'production'
+
+pidfile "/var/www/Wiki-Playlist/shared/tmp/pids/puma.pid"
+state_path "/var/www/Wiki-Playlist/shared/tmp/pids/puma.state"
+stdout_redirect '/var/www/Wiki-Playlist/shared/log/puma_access.log', '/var/www/Wiki-Playlist/shared/log/puma_error.log', true
+
+
+threads 2,4
+
+bind 'unix:///var/www/Wiki-Playlist/shared/tmp/sockets/puma.sock'
+
+workers 2
+
+
 
 preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+
+on_restart do
+  puts 'Refreshing Gemfile'
+  ENV["BUNDLE_GEMFILE"] = "/var/www/Wiki-Playlist/current/Gemfile"
+end
+
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
+    Que.mode = :async
   end
 end
